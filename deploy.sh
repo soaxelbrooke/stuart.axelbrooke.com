fe19bd8b084a808af9c1c30ea6095607eabfc4b9
Stuart Axelbrooke <stuart@axelbrooke.com>
2015-11-16 12:05:12 -0800

    Fix deploy script for python 27

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 
 URL='stuart.axelbrooke.com'
 
-source activate py27
-
 gsutil rsync -R -x '\.git|deploy' ./ gs://$URL
 gsutil acl ch -u AllUsers:R gs://$URL/*
 gsutil web set -m index.html gs://$URL
 
-source deactivate
 
