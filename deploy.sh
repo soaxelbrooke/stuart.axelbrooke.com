9f0c9b3905e4edb77ffcdfa9c86f3d58d274da71
Stuart Axelbrooke <stuart@axelbrooke.com>
2020-01-06 13:46:52 -0800

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,2 @@
 
-URL='stuart.axelbrooke.com'
-
-gsutil rsync -R -x '\.git|deploy' ./ gs://$URL
-gsutil acl ch -u AllUsers:R gs://$URL/*
-gsutil web set -m index.html gs://$URL
-
-
+scp index.html root@remote.axb:/root/code/stuart.axelbrooke.com/index.html
